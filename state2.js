96314b7892c1c7e356165eb6ae44f4794c755287
Kevin <Tor000020@gmail.com>
2017-05-08 05:39:48 -0400

    c
    c

--- state2.js
+++ state2.js
@@ -3,36 +3,57 @@ var demo = {}
 var centX = 1500 / 2;
 var centY = 1000 / 2;
 var frenk;
-var speed = 4;
+var speed = 5;
 demo.state2 = function(){};                   
-
-demo.state2.prototype = {                    
+demo.state2.prototype = {
+    
     preload: function(){
-        game.load.image('frenk', 'assets/sprites/frenk.png') 
+    game.load.image('frenk', 'assets/sprites/frenk.png') 
+    game.load.image('hilbg', 'assets/backgrounds/Hills.png')
     },                  
     create: function()
     { 
+//starts game physics engine
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+//defines event listeners for all stages and sets listeners
     addChangeStateEventListeners();
+//Sets game boundaries
+    game.world.setBounds(0, 0, 1500, 1000);
+//defines game scale
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; 
-    frenk = game.add.sprite(centX, centY, 'frenk')  
-    frenk.anchor.setTo(0.5, 0.5); 
-    game.stage.backgroundColor = '#80ff80';
-    console.log('state2');
+//sets the background image to hilbg
+    var hills = game.add.sprite(0,0, 'hilbg');
+//frenk - main player character
+        frenk = game.add.sprite(centX, centY, 'frenk');  
+        frenk.anchor.setTo(0.5, 0.5); 
+        frenk.scale.setTo(0.5, 0.5);
+        game.physics.enable(frenk);
+        frenk.body.collideWorldBounds=true;
+//camera details
+        game.camera.follow(frenk);
+        game.camera.deadzone = new Phaser.Rectangle(centX - 300, 0, 600, 1000 );
     },
     
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.D) || (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)))
-        {   frenk.x += speed;
+        { frenk.x += speed;
+         frenk.scale.setTo(0.5, 0.5);
         }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.A) || (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))){
-            frenk.x -= speed;
-        }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.W) || (game.input.keyboard.isDown(Phaser.Keyboard.UP) )){  
-            frenk.y -= speed;
-        }
-        else if(game.input.keyboard.isDown(Phaser.Keyboard.S) || (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))){
-            frenk.y += speed;
-        }
+        
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.A) || (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)))
+       { frenk.x -= speed;
+       frenk.scale.setTo(-0.5, 0.5);}
+      
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.W) || (game.input.keyboard.isDown(Phaser.Keyboard.UP) ))  
+            {frenk.y -= speed; 
+                if(frenk.y < 325) 
+                   {frenk.y = 325;}
+            }
+        
+        //
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.S) || (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)))
+        {  frenk.y += speed; }
+
     },                
 }
 
